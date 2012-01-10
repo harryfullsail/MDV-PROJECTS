bdaaad6ecdff3902f4495b255143c4f4bbba9cd9
Harry Lee <harrylee@Harry-Lees-MacBook-Pro.local>
2012-01-10 00:23:17 -0500

    Added files to the repo.

---
+++ lee_harry_assignment1.js
@@ -0,0 +1,38 @@
+// Deliverable 1!
+// Author: Harry Lee
+//01/05/2012
+// Created for: SDI Online
+// A Story That you Tale.
+
+// going to work at the library
+
+//var a = true,
+//    b = false,
+
+var work = "Going to work at the library.";
+               
+
+var mailRun = 100 + " books are send out to day. ";
+
+var daysWork = "Mail man pick up books from library. " +
+               " Books aer delivered to patrons homes. " +
+               " Patrons are happy! "; 
+ 
+
+var dayOff = "Take off from work to day.";
+
+var mailNotRun = 100 + " books are not pulled to day";
+
+var noPickUp = "Mail men don'\ t pick up books.";
+               
+var noWorkToDay = "No book are delivered to patrons homes." +
+                  " Patrons are not happy.";
+
+console.log( work );
+console.log( mailRun );
+console.log( daysWork);
+console.log( dayOff );
+console.log( mailNotRun );
+console.log( noPickUp );
+console.log( noWorkToDay );
+console.log
